a28f12c68f7ee47a92c12c133d67d0b2cd2e315a
sambu <trppsam@gmail.com>
2025-07-09 12:04:49 +0300

    intro to javascript

---
+++ script.js
@@ -0,0 +1,4 @@
+// TASK ONE: interptrated vs compiled
+// interpreted is dynamic
+// compiled is static
+//unlike html and css which we are compiled, js is interpreted
---
+++ variables.js
@@ -0,0 +1,35 @@
+// TASK ONE: interptrated vs compiled
+//TASK TWO:
+
+// variables are used to store data that can be used throughout the application.
+//they can be used to store configuration settings, user preferences, or any other data that needs to be accessed globally.
+
+
+// in js, we can create variables using the var, "let", and "const" keywords.
+
+
+// Keywords in programming languages are reserved words that have special meaning in the language.
+// they are used to define the structure and behaviour of the code.
+//  in Javascript, keywords include 'var', 'let', 'const', 'function', 'if', 'else', 'for', 'while', and many others.
+
+
+
+
+
+// var was the original way to declare variablrs in js, but it has been replaced by let and const becauseb of scope issues.
+var username = "John Doe";
+
+
+// let is used to declare variaables that can be reassigned later in the code.
+let age = 25;
+
+
+// const is used to declare variables that cannot be reassigned later in the code.it is also scoped.
+const pi = 3.14;
+
+const role = "distributor";
+let darkMode = false;
+
+// let variables can be reassigned, but const cannot be reassigned.
+age=31;
+pi=3.14159;
